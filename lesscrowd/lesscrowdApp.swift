f3533108b28ef690aafe3ba2cf3fc00d8205cd86
Akiem Moo <me@akiemmoo.com>
2025-01-16 08:34:38 +0800

    Initial Commit

---
+++ lesscrowd/lesscrowdApp.swift
@@ -0,0 +1,17 @@
+//
+//  lesscrowdApp.swift
+//  lesscrowd
+//
+//  Created by Akiem Moo on 16/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct lesscrowdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
