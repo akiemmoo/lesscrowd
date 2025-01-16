c766b7bf67d34f225b67e891e28f937649719d4b
Akiem Moo <me@akiemmoo.com>
2025-01-16 08:34:38 +0800

    Initial Commit

---
+++ lesscrowdTests/lesscrowdTests.swift
@@ -0,0 +1,17 @@
+//
+//  lesscrowdTests.swift
+//  lesscrowdTests
+//
+//  Created by Akiem Moo on 16/01/2025.
+//
+
+import Testing
+@testable import lesscrowd
+
+struct lesscrowdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
